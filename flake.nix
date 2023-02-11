dcf097149e67207b3ac1c61582aa44248efc8f1a
Sridhar Ratnakumar <srid@srid.ca>
2023-02-11 17:43:57 -0500

    No need for flake-parts

--- flake.nix
+++ flake.nix
@@ -1,23 +1,15 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    haskell-flake.url = "github:srid/haskell-flake";
-  };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
-      flake.haskellFlakeProjectModules.default = { pkgs, lib, ... }: {
-        overrides =
-          let
-            disableSeparateBinOutput =
-              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-          in
-          self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
-            ghcid = disableSeparateBinOutput super.ghcid;
-            ormolu = disableSeparateBinOutput super.ormolu;
-          };
-      };
+  outputs = { self }: {
+    haskellFlakeProjectModules.default = { pkgs, lib, ... }: {
+      overrides =
+        let
+          disableSeparateBinOutput =
+            pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+        in
+        self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
+          ghcid = disableSeparateBinOutput super.ghcid;
+          ormolu = disableSeparateBinOutput super.ormolu;
+        };
     };
+  };
 }
