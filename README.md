9d851e199a4c0c86b99c9f08a0e48af4a0e7589b
Sridhar Ratnakumar <srid@srid.ca>
2023-02-11 14:54:04 -0500

    Initial impl

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nixpkgs-140774-workaround
-A `haskell-flake` module to workaround https://github.com/NixOS/nixpkgs/issues/140774
+
+A [`haskell-flake`](https://github.com/srid/haskell-flake) module to workaround https://github.com/NixOS/nixpkgs/issues/140774
+
+## Who is using it?
+
+- haskell-template: https://github.com/srid/haskell-template/pull/85
+
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+      flake.haskellFlakeProjectModules.fixNixpkgs140774 = { pkgs, lib, ... }: {
+        overrides =
+          let
+            disableSeparateBinOutput =
+              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+          in
+          self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
+            ghcid = disableSeparateBinOutput super.ghcid;
+            ormolu = disableSeparateBinOutput super.ormolu;
+          };
+      };
+    };
+  }
