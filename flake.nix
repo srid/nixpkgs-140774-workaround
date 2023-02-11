1bbd741dc73b02c67c0e8207650879894679701c
Sridhar Ratnakumar <srid@srid.ca>
2023-02-11 14:55:56 -0500

    Expose the flake module

--- flake.nix
+++ flake.nix
@@ -8,16 +8,18 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
-      flake.haskellFlakeProjectModules.fixNixpkgs140774 = { pkgs, lib, ... }: {
-        overrides =
-          let
-            disableSeparateBinOutput =
-              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-          in
-          self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
-            ghcid = disableSeparateBinOutput super.ghcid;
-            ormolu = disableSeparateBinOutput super.ormolu;
-          };
+      flake.flakeModule = { self, config, lib, ... }: {
+        flake.haskellFlakeProjectModules.fixNixpkgs140774 = { pkgs, ... }: {
+          overrides =
+            let
+              disableSeparateBinOutput =
+                pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+            in
+            self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
+              ghcid = disableSeparateBinOutput super.ghcid;
+              ormolu = disableSeparateBinOutput super.ormolu;
+            };
+        };
       };
     };
-  }
+}
