66267ee43df370cdcc3166735ff04cfe0822f925
Sridhar Ratnakumar <srid@srid.ca>
2023-02-21 15:25:51 -0500

    Reformat

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   outputs = { self }: {
-    haskellFlakeProjectModules.default = { pkgs, lib, ... }: {
-      overrides =
-        let
-          disableSeparateBinOutput =
-            pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-        in
-        self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
-          ghcid = disableSeparateBinOutput super.ghcid;
-          ormolu = disableSeparateBinOutput super.ormolu;
-        };
+    haskellFlakeProjectModules = {
+      default = { pkgs, lib, ... }: {
+        overrides =
+          let
+            disableSeparateBinOutput =
+              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+          in
+          self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
+            ghcid = disableSeparateBinOutput super.ghcid;
+            ormolu = disableSeparateBinOutput super.ormolu;
+          };
+      };
     };
   };
 }
