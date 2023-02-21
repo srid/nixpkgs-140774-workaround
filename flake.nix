f08fe5886a16d48495b736926b030f515e3f4018
Sridhar Ratnakumar <srid@srid.ca>
2023-02-21 15:28:30 -0500

    Add new module exposing only patched packages

--- flake.nix
+++ flake.nix
@@ -12,6 +12,19 @@
             ormolu = disableSeparateBinOutput super.ormolu;
           };
       };
+      # For those scenarios your base package set is different, but you want
+      # ormolu and ghcid from default package set.
+      latestOrmoluAndGhcid = { pkgs, lib, ... }: {
+        devShell.tools = _:
+          let
+            disableSeparateBinOutput =
+              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+          in
+          with pkgs.haskellPackages; {
+            ormolu = disableSeparateBinOutput ormolu;
+            ghcid = disableSeparateBinOutput ghcid;
+          };
+      };
     };
   };
 }
