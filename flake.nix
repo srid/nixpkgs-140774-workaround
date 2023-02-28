0470b3cee6647b57337451e4b1221ef5dd9803ab
Sridhar Ratnakumar <srid@srid.ca>
2023-02-27 20:27:07 -0500

    Expose the default packages as is

--- flake.nix
+++ flake.nix
@@ -1,30 +1,26 @@
 {
-  outputs = { self }: {
-    haskellFlakeProjectModules = {
-      default = { pkgs, lib, ... }: {
-        overrides =
-          let
-            disableSeparateBinOutput =
-              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-          in
-          self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
-            ghcid = disableSeparateBinOutput super.ghcid;
-            ormolu = disableSeparateBinOutput super.ormolu;
-          };
+  outputs = { self }:
+    let
+      disableSeparateBinOutput = pkgs:
+        pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
+    in
+    {
+      haskellFlakeProjectModules = {
+        default = { pkgs, lib, ... }: {
+          overrides =
+            self: super: lib.optionalAttrs (pkgs.system == "aarch64-darwin") {
+              ghcid = disableSeparateBinOutput pkgs super.ghcid;
+              ormolu = disableSeparateBinOutput pkgs super.ormolu;
+            };
+        };
       };
       # For those scenarios your base package set is different, but you want
       # ormolu and ghcid from default package set.
-      latestOrmoluAndGhcid = { pkgs, lib, ... }: {
-        devShell.tools = _:
-          let
-            disableSeparateBinOutput =
-              pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-          in
-          with pkgs.haskellPackages; {
-            ormolu = disableSeparateBinOutput ormolu;
-            ghcid = disableSeparateBinOutput ghcid;
-          };
-      };
+      packages = { pkgs, ... }:
+        with pkgs.haskellPackages; {
+          ormolu = disableSeparateBinOutput pkgs ormolu;
+          ghcid = disableSeparateBinOutput pkgs ghcid;
+        };
     };
-  };
 }
+  
