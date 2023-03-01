40f8d7dacdf68faff5b79a32cac4890c9ab1df5a
Sridhar Ratnakumar <srid@srid.ca>
2023-02-28 21:05:45 -0500

    use package-settings

--- flake.nix
+++ flake.nix
@@ -1,28 +1,26 @@
 {
   outputs = { self }:
     let
-      disableSeparateBinOutput = pkgs:
-        pkgs.haskell.lib.compose.overrideCabal (_: { enableSeparateBinOutput = false; });
-      patch = pkgs: p:
-        if pkgs.system == "aarch64-darwin" then disableSeparateBinOutput pkgs p else p;
+      patch = { pkgs, old, ... }:
+        if pkgs.system == "aarch64-darwin" then { enableSeparateBinOutput = false; } else { };
+      patchDrv = pkgs: pkgs.haskell.lib.compose.overrideCabal (old: patch { inherit pkgs old; });
     in
     {
       inherit patch;
       haskellFlakeProjectModules = {
         default = { pkgs, lib, ... }: {
-          overrides =
-            self: super: {
-              ghcid = patch pkgs super.ghcid;
-              ormolu = patch pkgs super.ormolu;
-            };
+          packageSettings = {
+            ghcid.overrides = patch;
+            ormolu.overrides = patch;
+          };
         };
       };
       # For those scenarios your base package set is different, but you want
       # ormolu and ghcid from default package set.
       packages = { pkgs, ... }:
         with pkgs.haskellPackages; {
-          ormolu = patch pkgs ormolu;
-          ghcid = patch pkgs ghcid;
+          ormolu = patchDrv pkgs ormolu;
+          ghcid = patchDrv pkgs ghcid;
         };
     };
 }
